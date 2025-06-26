bf41c735797ba034538bc108fadfce813f39939b
Abhishek Rathore <79567263+abhish010102@users.noreply.github.com>
2025-06-26 18:56:04 +0530

    getDecimalValue

---
+++ linked-list/convert-binary-number-in-a-linked-list-to-integer.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getDecimalValue(head *ListNode) int {
+    var res int
+    for head != nil {
+        res = (res << 1) | head.Val
+        head = head.Next
+    }
+    return res
+}
